26aeb35916c44ab8830dca3f202735ccfa17bca6
Peter <truonganh20062001@gmail.com>
2023-02-14 12:48:15 +0700

    Pagination

--- front-end/src/pages/SearchPage/SearchPageBottom/Pagination/index.jsx
+++ front-end/src/pages/SearchPage/SearchPageBottom/Pagination/index.jsx
@@ -3,13 +3,28 @@ import React, { useEffect, useState } from 'react'
 import queryString from 'query-string'
 import { useHistory, useLocation } from 'react-router-dom'
 
-import { Pagination } from '@mui/material'
+import { DirectionsRun } from '@mui/icons-material'
+import { Box, IconButton, Pagination, TextField, Tooltip } from '@mui/material'
+
+import { AppStyles } from '~/constants/styles'
 
 const Paging = ({ size }) => {
     const history = useHistory()
     const { search: query, pathname } = useLocation()
     const { studysetname, sorttype, pageNumber, gradeid, subjectid } = queryString.parse(query)
     const [pageNum, setPageNumber] = useState(pageNumber ? +pageNumber : 1)
+    const [num, setNum] = useState(1)
+
+    const handleChange = (e) => {
+        const regex = /^[1-9\b]+$/
+        if (e.target.value === '' || regex.test(e.target.value)) {
+            setNum(e.target.value)
+        }
+    }
+
+    const pageSubmitHandler = () => {
+        setPageNumber(num)
+    }
 
     const pagingHandler = (__, value) => setPageNumber(value)
 
@@ -34,13 +49,55 @@ const Paging = ({ size }) => {
     }, [pageNum])
 
     return (
-        <Pagination
-            count={size}
-            variant="outlined"
-            sx={{ alignSelf: 'center', mt: 6 }}
-            onChange={pagingHandler}
-            page={pageNum}
-        />
+        <React.Fragment>
+            <Pagination
+                count={size}
+                variant="outlined"
+                sx={{ alignSelf: 'center' }}
+                onChange={pagingHandler}
+                page={pageNum}
+                showFirstButton
+                showLastButton
+            />
+            <Box
+                display="flex"
+                alignItems="center"
+                ml={2}
+                sx={{
+                    backgroundColor: AppStyles.colors['#FFFFFF'],
+                    border: '1px solid rgba(0, 46, 153, 0.3)',
+                    borderRadius: 1,
+                    height: 40,
+                }}
+                justifyContent="space-between"
+            >
+                <Box component="form" noValidate autoComplete="off" sx={{ width: 30, pl: 1 }}>
+                    <TextField variant="standard" type="text" onChange={(e) => handleChange(e)} value={num} />
+                </Box>
+
+                <Tooltip title="Đi đến" followCursor>
+                    <IconButton
+                        size="small"
+                        sx={{
+                            height: 40,
+                            width: 40,
+                            ml: 1,
+                            borderRadius: 1,
+                            color: AppStyles.colors['#FFFFFF'],
+                            backgroundColor: AppStyles.colors['#004DFF'],
+                            borderLeft: '1px solid rgba(0, 46, 153, 0.3)',
+                            ':hover': {
+                                bgcolor: AppStyles.colors['#0045e5'],
+                                color: 'white',
+                            },
+                        }}
+                        onClick={pageSubmitHandler}
+                    >
+                        <DirectionsRun fontSize="medium" />
+                    </IconButton>
+                </Tooltip>
+            </Box>
+        </React.Fragment>
     )
 }
 
